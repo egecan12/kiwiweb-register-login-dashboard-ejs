daeb1736da0972e883f45829b84a861c0929e667
egecan12 <egecan18k@gmail.com>
2020-06-22 15:51:26 -0400

    loading animation fixed

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var element = document.getElementById('myDiv');
 
 setTimeout(function(){
     document.getElementById('myDiv').style.display ="none"
+    document.getElementById('body').style.overflowY = "scroll"
+
 }, 1500);
